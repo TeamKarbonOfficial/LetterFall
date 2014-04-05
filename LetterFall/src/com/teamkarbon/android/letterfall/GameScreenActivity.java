247054beb3cbdfab9c174c1539dff32baa9017ed
euwbah <euwbah@gmail.com>
2014-04-05 19:40:47 +0800

    Added free use font style for app, changed layout, added title of game, added new activity (the main game screen) TODO: Use the new font using getAssets()

---
+++ LetterFall/src/com/teamkarbon/android/letterfall/GameScreenActivity.java
@@ -0,0 +1,12 @@
+package com.teamkarbon.android.letterfall;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameScreenActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+}
