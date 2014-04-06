7fcde84aa06d00b178cb002dd0d69dfdeeb3573a
darkpolice <joe@teamkarbon.com>
2014-04-06 21:49:18 +0800

    Fixed warning for VerisonName Removed ActionBar Added Play Button and Listener

--- LetterFall/src/com/teamkarbon/android/letterfall/MainActivity.java
+++ LetterFall/src/com/teamkarbon/android/letterfall/MainActivity.java
@@ -2,14 +2,37 @@ package com.teamkarbon.android.letterfall;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	//Declare
+	Button ButtonPlay;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Initialize
+		ButtonPlay = (Button) findViewById(R.id.ButtonPlay);
+		
+		addListenerOnButtonPlay();
+	}
+	
+	private void addListenerOnButtonPlay() {
+		ButtonPlay.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				//My responsibility to fix this :) Leave it as a comment for now :P
+				//Intent aboutintent = new Intent(this, AboutActivity.class);
+				//startActivity(aboutintent);
+			}
+		});
 	}
 
 	@Override
